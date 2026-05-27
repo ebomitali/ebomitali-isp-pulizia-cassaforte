de795b1e668c3f2f3de1ff0b4adfaaa7581a16d3
Bomitali Evelino <evelino.bomitali@hogwart.it>
2026-05-27 10:05:10 +0200

    Rename full source file

--- full-fat-source/src/main/groovy/FullPuliziaCassaforte.groovy
+++ full-fat-source/src/main/groovy/FullPuliziaCassaforte.groovy
@@ -1363,6 +1363,7 @@ class ZosFileOpsUSS implements ZosFileOps {
 // --- ScriptLoaderHelper.groovy (script) ---
 // This code is meaningful only for groovy scripts loaded via ScriptLoader.loadScript()
 // from another script run in USS by groovyz
+// It will be appended to the end of FullPuliziaCassaforte.groovy
 
 def createPuliziaCassaforteImpl() {
     return PuliziaCassaforteImpl.newInstance()
--- library/src/zos/groovy/ScriptLoaderHelper.groovy
+++ library/src/zos/groovy/ScriptLoaderHelper.groovy
@@ -1,5 +1,6 @@
 // This code is meaningful only for groovy scripts loaded via ScriptLoader.loadScript()
 // from another script run in USS by groovyz
+// It will be appended to the end of FullPuliziaCassaforte.groovy
 
 def createPuliziaCassaforteImpl() {
     return PuliziaCassaforteImpl.newInstance()
