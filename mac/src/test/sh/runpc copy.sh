9696417f5b8fc2edd81c815c79db830e534574ec
Bomitali Evelino <evelino.bomitali@hogwart.it>
2026-07-27 10:54:14 +0200

    Remove duplicated

--- mac/src/test/sh/runpc copy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-groovyz RunPC.groovy
-echo "Res: $?"
--- mac/src/test/sh/test copy.groovy
+++
@@ -1,4 +0,0 @@
-import org.slf4j.LoggerFactory
-// Works with groovyz
-def log = LoggerFactory.getLogger(this.class)
-log.info "Hello from test.groovy script..."
